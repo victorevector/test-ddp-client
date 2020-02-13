ae86a9af376651a449304d9ccc19e17cb05214bd
VEF <victorevector@gmail.com>
2020-02-12 19:56:04 -0800

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+const DDPClient = require("ddp");
+const readline = require("readline");
+const EMAIL = "victor@treestack.io";
+const PASSWORD = "xawcym-9mukze-fubwaS";
+
+
+// read from terminal 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout, 
+    terminal: false
+});
+
+// ddp client object
+const ddpclient = new DDPClient({
+    autoReconnect: true, 
+    autoReconnectTimer: 500,
+    ddpVersion: '1',
+    url: "ws://localhost:3000/websocket"
+});
+
+// establish a connection 
+ddpclient.connect((error, wasReconnect) => {
+    if (error) {
+        console.log("ERROR!");
+        return;
+    }
+    if (wasReconnect) {
+        console.log("Reestablished connection");
+    }
+    console.log("connected!");
+
+    login(ddpclient, EMAIL, PASSWORD);
+
+    triggerCallMethod(rl, () => callExportMethod(ddpclient) );
+});
+
+
+
+//HELPER FUNCTIONS
+
+function login(client, username, password) {
+    client.call("login", [{ user: { email: username }, password: password}], 
+    (err, result) => {
+        if (err) console.log("Error!", err);
+        console.log("Result: ", result);
+    });
+}
+
+function callExportMethod(client) {
+    client.call(
+        "reports.test.doc",
+        ["This is a parameter"],
+        (err, result) => {
+            if (err) {
+                console.log("export method called. ERROR", err);
+            }
+            console.log("export method called! Result: ", result);
+        }
+    );
+}
+
+function triggerCallMethod(rl, cb) {
+    rl.on('line', (line) => {
+        console.log("calling method...");
+        cb();
+    });
+}
